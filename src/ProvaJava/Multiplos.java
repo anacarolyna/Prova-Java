0d3eedad0134caa7c1d7193d77c693d470c0f30f
Ana Carolyna Diniz <102266958+anacarolyna@users.noreply.github.com>
2022-11-12 11:03:04 -0300

    Update Multiplos.java

--- src/ProvaJava/Multiplos.java
+++ src/ProvaJava/Multiplos.java
@@ -4,13 +4,13 @@ public class Multiplos {
 	public static void main(String[]args){
 		
 		for(int i=1; i<=100; i ++){
-			if(i % 3 ==0){
+			if (i % 3 ==0){
 				System.out.println("Foo");
 			}
-			if(i % 5 ==0){
+			else if (i % 5 ==0){
 					System.out.println("Baa");
 			}
-			if (i % 3 ==0 && i % 5 ==0){
+			else (i % 3 ==0 && i % 5 ==0){
 					System.out.println("FooBaa");
 			}
 				
